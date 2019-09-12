3f59eb8849f9ceeaf45ad2c2d312e4845c8c154e
andrew <tayljame@musc.edu>
2019-09-12 19:02:26 -0400

    added a new tool

---
+++ scripts/align_tool.m
@@ -0,0 +1,42 @@
+function [new_source,shift]=align_tool(target_object_seg,source_obj_seg,use_CoM)
+%BIG NOTE: RIGHT NOW, THIS IS ONLY INTENDED TO BE USED ON images with the
+%same dimensions (eg two instances of the same sequence)
+
+%target object seg should be a dwi object with one element. 
+if length(target_object_seg)>1; error('unintended usage');end
+
+%ensure correct usage
+assert(all(target_object_seg.hdr.dim == source_obj_seg(1).hdr.dim))
+
+
+%% move to align CoM if user has specified so
+
+shift=eye(4); %to be used if ~use_CoM
+if use_CoM
+    trg_CoM=centerOfMass(target_object_seg.img);
+    src_CoM=centerOfMass(source_object_seg(1).img);
+    
+    %create 4x4 of required shift
+    src_shift=trg_CoM-src_CoM;
+    shift=spm_matrix(src_shift);
+end
+
+trg_space=target_object_seg.hdr.mat;
+for j = 1:length(source_obj_seg)
+    %next line causes (in spm_reslice) source voxels to shift, then source
+    %hdr.mat to be set to target
+    source_obj_seg(j).hdr.mat=trg_space*shift;
+    
+    %set the others to the same
+    source_obj_seg(j).hdr.private.mat=source_obj_seg(j).hdr.mat;
+    source_obj_seg(j).hdr.private.mat0=source_obj_seg(j).hdr.mat;
+end
+
+big_obj=join_obj(target_object_seg,source_obj_seg);
+if use_CoM % you don't need to reslice if you're not moving anything
+    big_obj=spm_reslice_at(big_obj,wrtflg);%inputs need to be target,source,other
+end
+new_source=big_obj(2:end);
+
+
+end
