ca4975110dec3450c11b9430898226c7d6d9a07f
andrew <tayljame@musc.edu>
2019-09-17 17:46:33 -0400

    works now

--- make_flags.m
+++ make_flags.m
@@ -5,28 +5,35 @@ p_init=inputParser;
 addRequired(p_init,'read_write_str', @(str) sum(strcmpi({'read','write'},str))==1 )
 parse(p_init,read_write_str)
 read_write_str=p_init.Results.read_write_str;
-
+clear p_init
 p=inputParser;
 
+%annoyingly, I have to do this again or it'll mess up
+addRequired(p,'read_write_str', @(str) sum(strcmpi({'read','write'},str))==1 )
+%in this weird case it's actually useful for them not to programmatically
+%assign things to my variables....
+
 if strcmpi(read_write_str,'read')
-    addOptional(p,'pick','',@(x) ischar(x)||isstring(x) )
-    addOptional(p,'b0',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
-    addOptional(p,'no','',@(x) ischar(x)||isstring(x) )
+    addParameter(p,'pick','',@(x) ischar(x)||isstring(x) )
+    addParameter(p,'b0',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
+    addParameter(p,'no','',@(x) ischar(x)||isstring(x) )
     %the 'no' option is going to become noload and nofns
 
     
     
 elseif strcmpi(read_write_str,'write')
     
-    addOptional(p,'dt',1, @(x) isequal(x,1) || isequal(x,-1) || (isequal(x(1),0) && sum(x(2)==[256 2 4 512 8 768 16 64])==1))
-    addOptional(p,'gr',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
-    addOptional(p,'vol',4,@(x) isnumeric(x) && sum(x==[3 4])==1 )
-    addOptional(p,'nfn',0,@(x) isnumeric(x) && sum(x==[0 1])==1 )
+    addParameter(p,'dt',1, @(x) isequal(x,1) || isequal(x,-1) || (isequal(x(1),0) && sum(x(2)==[256 2 4 512 8 768 16 64])==1))
+    addParameter(p,'gr',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
+    addParameter(p,'vol',4,@(x) isnumeric(x) && sum(x==[3 4])==1 )
+    addParameter(p,'nfn',0,@(x) isnumeric(x) && sum(x==[0 1])==1 )
     
 end
 
-parse(p,varargin{:})
+parse(p,read_write_str,varargin{:})
 flds=fieldnames(p.Results);
 for i=1:numel(flds)
-    flags.(flds{i})=p.Results.(flds{i});
+    if ~strcmpi(flds{i},'read_write_str') %this is a meta variable, don't want it in the output
+        flags.(flds{i})=p.Results.(flds{i});
+    end
 end
