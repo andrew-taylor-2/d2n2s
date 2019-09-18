886359e19b1f4e03805a66e3db01077f68265e1f
andrew <tayljame@musc.edu>
2019-09-18 14:34:58 -0400

    adds flags for coregistration

--- make_flags.m
+++ make_flags.m
@@ -2,10 +2,11 @@ function flags = make_flags(read_write_str,varargin)
 
 %make sure you know whether you're reading or writing
 p_init=inputParser;
-addRequired(p_init,'read_write_str', @(str) sum(strcmpi({'read','write'},str))==1 )
+addRequired(p_init,'read_write_str', @(str) sum(strcmpi({'read','write','coregister'},str))==1 )
 parse(p_init,read_write_str)
 read_write_str=p_init.Results.read_write_str;
 clear p_init
+% input parser for the flags themselves
 p=inputParser;
 
 %annoyingly, I have to do this again or it'll mess up
@@ -14,22 +15,28 @@ addRequired(p,'read_write_str', @(str) sum(strcmpi({'read','write'},str))==1 )
 %assign things to my variables....
 
 if strcmpi(read_write_str,'read')
+
     addParameter(p,'pick','',@(x) ischar(x)||isstring(x) )
-    addParameter(p,'b0',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
+    addParameter(p,'b0',1,@(x) isnumeric(x) && sum(x==[0 1])==1 ) %prolly slower alt check: @(x) isnumeric(x) && sum(x==[0 1])==1 
     addParameter(p,'no','',@(x) ischar(x)||isstring(x) )
     %the 'no' option is going to become noload and nofns
 
-    
-    
 elseif strcmpi(read_write_str,'write')
     
     addParameter(p,'dt',1, @(x) isequal(x,1) || isequal(x,-1) || (isequal(x(1),0) && sum(x(2)==[256 2 4 512 8 768 16 64])==1))
-    addParameter(p,'gr',1,@(x) isnumeric(x) && sum(x==[0 1])==1 )
-    addParameter(p,'vol',4,@(x) isnumeric(x) && sum(x==[3 4])==1 )
-    addParameter(p,'nfn',0,@(x) isnumeric(x) && sum(x==[0 1])==1 )
+    addParameter(p,'gr',1,@(x) isequal(x,1) || isequal(x,0) )
+    addParameter(p,'vol',4,@(x) isequal(x,3) || isequal(x,4) )
+    addParameter(p,'nfn',0,@(x) isequal(x,1) || isequal(x,0) )
+    
+elseif strcmpi(read_write_str,'coregister')
+    
+    addParameter(p,'no0',0, @(x) isequal(x,1) || isequal(x,0) )
+    addParameter(p,'animal',0, @(x) isequal(x,1) || isequal(x,0) )
+    addParameter(p,'apply',2, @(x) isnumeric(x) && sum(x==[0 1 2])==1 )
     
 end
 
+%grab the parsed inputs and assign them to flags.()
 parse(p,read_write_str,varargin{:})
 flds=fieldnames(p.Results);
 for i=1:numel(flds)
