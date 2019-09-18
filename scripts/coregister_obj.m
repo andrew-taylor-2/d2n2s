259029a40547ff106bdc068bc7ba1fd1438576b9
andrew <tayljame@musc.edu>
2019-09-18 19:10:35 -0400

    adds ~just~ reslice option

--- scripts/coregister_obj.m
+++ scripts/coregister_obj.m
@@ -6,9 +6,18 @@ function [new_source,M]=coregister_obj(target_object_seg,source_obj_seg,flags)
 
 %     : animal: if 1 (0 default), x10 scaled down parameters will be used for spm_coreg. This is very important if your voxel sizes are small (e.g. closer to .1x.1.x.1 than 1.5x1.5x1.5).
 
-%     : apply: if 2 (default), affine registration will be estimated and image will be resliced thusly. If 1, reg will be estimated and the orientation matrix will be updated to reflect the change of origin. If 0, "new_source" will be equal to source_obj_seg and M will be the estimated rigid body reg.
+%     : apply: if 2 (default), affine registration will be estimated and
+%     image will be resliced thusly. If 1, reg will be estimated and the
+%     orientation matrix will be updated to reflect the change of origin.
+%     If 0, "new_source" will be equal to source_obj_seg and M will be the
+%     estimated rigid body reg. If -1, source_obj_seg will be resliced but
+%     not moved to match target_object_seg
 
 if length(target_object_seg)>1; error('unintended usage');end
+
+if ~exist('flags','var') || ~isfield(flags,'apply')
+    flags.apply=2;
+end
 
 % coregistration and reslicing parameters
 estflg.cost_fun = 'nmi';
@@ -19,58 +28,56 @@ wrtflg        = spm_get_defaults('realign.write');
 wrtflg.interp   = 1;
 wrtflg.which    = [1 0];
 
-if isfield(flags,'animal') && isequal(1,flags.animal)
-    estflg.sep=estflg.sep*0.1;
-    estflg.fwhm=estflg.fwhm*0.1;
-end
-	
-
-target_object_seg.hdr.img=target_object_seg.img;
-for i=1:length(source_obj_seg)
-    source_obj_seg(i).hdr.img=source_obj_seg(i).img;
-end
-
-
-if ~isfield(flags,'no0') || isequal(flags.no0,0)
-    x  = spm_coreg_at(target_object_seg.hdr, source_obj_seg(1).hdr, estflg); 
-else 
-    x  = spm_coreg_at_no0(target_object_seg.hdr, source_obj_seg(1).hdr, estflg);
-end
-
-%unset .hdr.img for clarity 
-target_object_seg.hdr=rmfield(target_object_seg.hdr,'img');
-for i=1:length(source_obj_seg)
-    source_obj_seg(i).hdr=rmfield(source_obj_seg(i).hdr,'img');
-end
-
-
-M  = inv(spm_matrix(x));
-
-if ~isfield(flags,'apply') 
-    flags.apply=2;
-end
-
-if flags.apply>0
-
-    MM = zeros(4, 4, length(source_obj_seg));
-
-    for j=1:length(source_obj_seg)
-        MM(:,:,j)=source_obj_seg(j).hdr.mat;
+if ~isequal(flags.apply,-1) % if the user doesn't want to completely skip coreg and get to reslicing
+    
+    if isfield(flags,'animal') && isequal(1,flags.animal)
+        estflg.sep=estflg.sep*0.1;
+        estflg.fwhm=estflg.fwhm*0.1;
     end
-    for j = 1:length(source_obj_seg)
-        source_obj_seg(j).hdr.mat=M*MM(:,:,j);
-        source_obj_seg(j).hdr.private.mat=source_obj_seg(j).hdr.mat;
-        source_obj_seg(j).hdr.private.mat0=source_obj_seg(j).hdr.mat;
+    
+    
+    target_object_seg.hdr.img=target_object_seg.img;
+    for i=1:length(source_obj_seg)
+        source_obj_seg(i).hdr.img=source_obj_seg(i).img;
     end
+    
+    
+    if ~isfield(flags,'no0') || isequal(flags.no0,0)
+        x  = spm_coreg_at(target_object_seg.hdr, source_obj_seg(1).hdr, estflg);
+    else
+        x  = spm_coreg_at_no0(target_object_seg.hdr, source_obj_seg(1).hdr, estflg);
+    end
+    
+    %unset .hdr.img for clarity
+    target_object_seg.hdr=rmfield(target_object_seg.hdr,'img');
+    for i=1:length(source_obj_seg)
+        source_obj_seg(i).hdr=rmfield(source_obj_seg(i).hdr,'img');
+    end
+    
+    M  = inv(spm_matrix(x));
 
-    if isequal(flags.apply,1) %if you only want hdr to change, bail out here
+    if flags.apply>0
+        
+        MM = zeros(4, 4, length(source_obj_seg));
+        
+        for j=1:length(source_obj_seg)
+            MM(:,:,j)=source_obj_seg(j).hdr.mat;
+        end
+        for j = 1:length(source_obj_seg)
+            source_obj_seg(j).hdr.mat=M*MM(:,:,j);
+            source_obj_seg(j).hdr.private.mat=source_obj_seg(j).hdr.mat;
+            source_obj_seg(j).hdr.private.mat0=source_obj_seg(j).hdr.mat;
+        end
+        
+        if isequal(flags.apply,1) %if you only want hdr to change, bail out here
+            new_source=source_obj_seg;
+            return;
+        end
+    else %if flags.apply is 0, i.e. you just wanted to see what M was, leave source unchanged and bail out here
         new_source=source_obj_seg;
-	return;
-    end	
-else %if flags.apply is 0, i.e. you just wanted to see what M was, leave source unchanged and bail out here
-    new_source=source_obj_seg;
-    return
-end
+        return
+    end
+end % endif flags.apply~=-1
 
 %implicit if: other options have bailed at this point
 %if ~isfield(flags,'apply') || isequal(flags.apply,2)
