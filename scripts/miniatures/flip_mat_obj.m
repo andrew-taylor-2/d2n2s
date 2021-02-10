8c24efedfd003d0c2a106602b90821a9867342ee
Andrew Taylor <tayljame@musc.edu>
2021-02-10 10:45:04 -0500

    adds useful comments

--- scripts/miniatures/flip_mat_obj.m
+++ scripts/miniatures/flip_mat_obj.m
@@ -1,11 +1,14 @@
 function [out_obj,flip_mat]=flip_mat_obj(in_obj,dim)
+%if you have off diagonal values, this might not work (But it does if
+%you're in MNI space). What I really should do is use spm_imatrix, then
+%flip the scaling factor. 
 
-flip_mat=eye(4);
-if dim==1, ind=1; elseif dim==2, ind=6; elseif dim==3, ind=11; else, error;end
+flip_mat=eye(4); %start with identity matrix
+if dim==1, ind=1; elseif dim==2, ind=6; elseif dim==3, ind=11; else, error;end %change sign of one of the diagonal elements
 flip_mat(ind)=-1;
 
 in_obj_flipped=in_obj;
-in_obj_flipped.hdr.mat=flip_mat*in_obj_flipped.hdr.mat;
+in_obj_flipped.hdr.mat=flip_mat*in_obj_flipped.hdr.mat; %use what we just made to flip the orientation matrix
 
-out_obj=coregister_obj(in_obj,in_obj_flipped,make_flags('coregister','apply',-1));
+out_obj=coregister_obj(in_obj,in_obj_flipped,make_flags('coregister','apply',-1)); %this command essentially does spm_reslice -- the final image will end up with the orient. matrix of the original image (not flipped). Because the "source" and "target" have orientations a 180 degree rotation apart, spm_reslice will flip "source" 180 degrees in the dimension you chose
 end
