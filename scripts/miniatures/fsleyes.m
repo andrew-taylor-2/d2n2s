aa40d7177518b706935184ad8fa82788d7f001b8
James Andrew Taylor <taylora5@g.cofc.edu>
2021-05-21 14:18:03 -0400

    incremental

--- scripts/miniatures/fsleyes.m
+++ scripts/miniatures/fsleyes.m
@@ -1,4 +1,12 @@
 function fsleyes(DWI_obj)
 %simple, effective. Probably.
-system(['fsleyes ' DWI_obj(1).fns.nii])
+% system(['fsleyes ' DWI_obj(1).fns.nii])
+if length(DWI_obj)==1
+    cmd=['fsleyes ' DWI_obj(1).fns.nii];
+else
+    fn_cellstr=arrayfun(@(x) x.fns.nii,DWI_obj);
+    cmd=['fsleyes ' sprintf('%s ',string(fn_cellstr))];
+    disp(cmd)
+end
+system(cmd)
 end
